f3ecb64e2b1ac792e294a1ebe42e2fbf30da05c8
GabrielShumanski <gabrielshuamsnki@gmail.com>
2023-05-19 23:15:59 +0300

    deleted TimeAuthenticationImplementation

--- hw1/test.js
+++ hw1/test.js
@@ -1,10 +1,10 @@
 import { readFileSync } from 'fs';
 
 async function example(){
-    try{
+    try {
         let data = readFileSync('D:\\Programs\\novarto\\hw1\\input.txt', 'utf8');
         for (let i = 0, j = 0; i < data.length + 1; ++i) {
-            if(data.charAt(i) == '\n') {
+            if (data.charAt(i) == '\n') {
                 let inputLine = data.slice(j, i);
                 inputLine = inputLine.split(" ");
                 console.log(inputLine);
